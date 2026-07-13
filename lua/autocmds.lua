a884af7c45f96a3be1229eb4ea65a0969392aebb
kar1221 <khyeong191@gmail.com>
2026-07-13 14:46:48 +0800

    change identation for several languages

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,21 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_augroup("setIndent", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "setIndent",
+  pattern = {
+    "css",
+    "html",
+    "javascript",
+    "lua",
+    "markdown",
+    "md",
+    "typescript",
+    "scss",
+    "xml",
+    "xhtml",
+    "yaml",
+  },
+  command = "setlocal shiftwidth=2 tabstop=2",
+})
+
