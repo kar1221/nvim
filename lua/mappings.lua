bc39e37cb0d9a0e6ddfc44471210ed1db11e1062
kar1221 <khyeong191@gmail.com>
2026-07-19 15:38:57 +0800

    add some mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -23,9 +23,18 @@ map("n", "<leader>e", function()
   require("nvim-tree.api").tree.toggle()
 end, { desc = "Toggle NvimTree" })
 
+map("n", "gI", function ()
+  vim.lsp.buf.implementation()
+end, { desc = "Goto Implementation" })
+
 map({ "n", "v" }, "<leader>cf", function()
   require("conform").format()
 end, { desc = "Format Buffer" })
+
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
 
@@ -66,5 +75,25 @@ map("n", "<leader>ca", function()
   require("actions-preview").code_actions()
 end, { desc = "Code action" })
 
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.name == "typescript-tools" then
+      local map = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
+      end
+
+      map("<leader>co", "<cmd>TSToolsOrganizeImports<cr>", "Organize Imports")
+      map("<leader>cs", "<cmd>TSToolsSortImports<cr>", "Sort Imports")
+      map("<leader>cU", "<cmd>TSToolsRemoveUnusedImports<cr>", "Remove Unused Imports")
+      map("<leader>cu", "<cmd>TSToolsRemoveUnused<cr>", "Remove Unused")
+      map("<leader>cm", "<cmd>TSToolsAddMissingImports<cr>", "Add Missing Imports")
+      map("<leader>cg", "<cmd>TSToolsGoToSourceDefinition<cr>", "Go to Source Definition")
+      map("<leader>cR", "<cmd>TSToolsRenameFile<cr>", "Rename File")
+      map("<leader>cF", "<cmd>TSToolsFileReferences<cr>", "File References")
+    end
+  end,
+})
