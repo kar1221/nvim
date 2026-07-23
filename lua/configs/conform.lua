667ae080317544cf4ee3f72626382532367531c4
kar1221 <khyeong191@gmail.com>
2026-07-23 15:58:05 +0800

    enable format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -18,11 +18,11 @@ local options = {
     javascript = { "prettier" }
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
