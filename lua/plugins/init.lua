0f47c4ecb4320da144e0ee99708df33adea4250e
kar1221 <khyeong191@gmail.com>
2026-07-25 14:40:03 +0800

    try out default keymap preset in blink

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,31 +46,7 @@ return {
         },
       },
       keymap = {
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+        preset = "default",
       },
       completion = {
         menu = {
@@ -128,7 +104,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       indent = {
-        enabled = true,
+        disable = { "dart" },
+        enabled = false,
       },
       ensure_installed = {
         "vim",
@@ -140,6 +117,7 @@ return {
         "svelte",
         "bash",
         "c",
+        "dart",
         "cmake",
         "cpp",
         "css",
