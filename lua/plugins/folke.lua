271346de7c9979fe004593c9dffc28189545d6d9
kar1221 <khyeong191@gmail.com>
2026-07-27 17:34:30 +0800

    add flash.nvim

--- lua/plugins/folke.lua
+++ lua/plugins/folke.lua
@@ -1,5 +1,54 @@
 return {
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
+
+  {
     "folke/trouble.nvim",
     opts = {},
     cmd = "Trouble",
@@ -66,13 +115,13 @@ return {
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
-      {
-        "rcarriga/nvim-notify",
-        opts = {
-          render = "compact",
-          stages = "slide",
-        },
-      },
+      -- {
+      --   "rcarriga/nvim-notify",
+      --   opts = {
+      --     render = "compact",
+      --     stages = "slide",
+      --   },
+      -- },
     },
     opts = {
       lsp = {
