bd78dc68aee14bb85afa2234240e41eac11c5715
kar1221 <khyeong191@gmail.com>
2026-07-30 22:00:39 +0800

    disable markdonw rendering for floating windows

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -8,6 +8,29 @@ return {
     require("render-markdown").setup {
       preset = "obsidian",
 
+      ignore = function(buf)
+        if vim.bo[buf].buftype ~= "nofile" then
+          return false
+        end
+
+        -- Disable Markdown Render for floating windows
+        -- Lsp Signature, nvim-notify etc
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buf then
+            local cfg = vim.api.nvim_win_get_config(win)
+            if cfg.relative ~= "" then
+              return true
+            end
+          end
+        end
+
+        return false
+      end,
+
+      -- document = {
+      --   enabled = false,
+      -- },
+
       completions = {
         lsp = {
           enabled = true,
