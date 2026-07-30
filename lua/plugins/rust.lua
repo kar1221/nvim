b29ce549584479530c6927c7f697aa915644e130
kar1221 <khyeong191@gmail.com>
2026-07-30 21:59:49 +0800

    move rustaceanvim config to its plugin file

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -20,5 +20,47 @@ return {
   {
     "mrcjkb/rustaceanvim",
     ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(_, bufnr)
+            -- vim.keymap.set("n", "K", function()
+            --   vim.cmd "RustLsp hover actions"
+            -- end, { desc = "Code Action", buffer = bufnr })
+          end,
+          default_settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              checkOnSave = true,
+              diagnostics = true,
+              procMacro = {
+                enable = true,
+              },
+              files = {
+                exclude = {
+                  ".direnv",
+                  ".git",
+                  ".jj",
+                  ".github",
+                  ".gitlab",
+                  "bin",
+                  "node_modules",
+                  "target",
+                  "venv",
+                  ".venv",
+                },
+                watcher = "client",
+              },
+            },
+          },
+        },
+      }
+    end,
   },
 }
